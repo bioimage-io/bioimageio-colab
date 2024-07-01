b02ea50ec166c72f9fd988cc9a86c13b1c1afdc7
Nils Mechtel <nils.mech@gmail.com>
2024-07-01 13:31:08 +0200

    store token in .env file

---
+++ bioimageio_colab/generate_token.py
@@ -0,0 +1,22 @@
+import os
+
+from imjoy_rpc.hypha.sync import login
+
+
+def main():
+    # Login to hypha
+    token = login({"server_url": "https://ai.imjoy.io"})
+
+    # Save the token to a .env file
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+    with open(file_path, "w") as env_file:
+        env_file.write(f'USER_TOKEN="{token}"')
+
+    # Define the permission mode for read and write for the owner only
+    mode = 0o600
+
+    # Change the file permissions
+    os.chmod(file_path, mode)
+
+if __name__ == "__main__":
+    main()
