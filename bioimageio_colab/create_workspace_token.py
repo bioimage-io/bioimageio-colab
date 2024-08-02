bcb9881d5385c8c1348353813899aa3a228decd2
Nils Mechtel <nils.mech@gmail.com>
2024-08-02 20:21:06 +0200

    create persistent bioimageio-colab workspace

---
+++ bioimageio_colab/create_workspace_token.py
@@ -0,0 +1,140 @@
+import os
+import argparse
+from hypha_rpc import connect_to_server, login
+import asyncio
+
+
+async def create_workspace_token(args):
+    # Get a user login token
+    token = await login({"server_url": args.server_url})
+
+    # Connect to the Hypha server
+    user = await connect_to_server(
+        {
+            "server_url": args.server_url,
+            "token": token,
+        }
+    )
+
+    # Check if the workspace already exists
+    exists = any(
+        [
+            args.workspace_name == workspace["name"]
+            for workspace in await user.list_workspaces()
+        ]
+    )
+
+    # Create a workspace
+    if not exists or args.overwrite:
+        workspace = await user.create_workspace(
+            {
+                "name": args.workspace_name,
+                "description": args.description,
+                "owners": args.owners,  # user ID of workspace owner is added automatically
+                "allow_list": args.allow_list,
+                "deny_list": args.deny_list,
+                "visibility": "public",  # public/protected
+                "persistent": True,  # keeps the workspace alive even after a server restart
+            },
+            overwrite=args.overwrite,  # overwrites if the workspace already exists
+        )
+        # Check if the workspace was created
+        assert any(
+            [
+                args.workspace_name == workspace["name"]
+                for workspace in await user.list_workspaces()
+            ]
+        )
+        print(f"Workspace created: {workspace['name']}")
+
+    # Generate a workspace token
+    token = await user.generate_token(
+        {
+            "workspace": args.workspace_name,
+            "expires_in": args.token_expires_in,
+            "permission": "read_write",
+        }
+    )
+    expires_in_days = token["expires_in"] / 60 / 60 / 24
+    print(
+        f"Workspace token generated ({token['permission']} - expires in {expires_in_days} days):\n{token['token']}"
+    )
+
+    # Save the token to a .env file
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+
+    # Read existing .env file if it exists
+    updated_env_lines = []
+    found_token_line = False
+    if os.path.exists(file_path):
+        with open(file_path, "r") as env_file:
+            for line in env_file:
+                if line.strip().startswith("HYPHA_TOKEN="):
+                    updated_env_lines.append(f'HYPHA_TOKEN="{token}"\n')
+                    found_token_line = True
+                else:
+                    updated_env_lines.append(line)
+
+    # If HYPHA_TOKEN line wasn't found, add it
+    if not found_token_line:
+        updated_env_lines.append(f'HYPHA_TOKEN="{token}"\n')
+
+    # Write back to the .env file
+    with open(file_path, "w") as env_file:
+        env_file.writelines(updated_env_lines)
+
+    # Define the permission mode for read and write for the owner only
+    mode = 0o600
+
+    # Change the file permissions
+    os.chmod(file_path, mode)
+
+    print(f"Token saved to {file_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create the BioImageIO Colab workspace."
+    )
+    parser.add_argument(
+        "--server_url",
+        default="https://hypha.aicell.io",
+        help="URL of the Hypha server",
+    )
+    parser.add_argument(
+        "--workspace_name", default="bioimageio-colab", help="Name of the workspace"
+    )
+    parser.add_argument(
+        "--description",
+        default="The BioImageIO Colab workspace for serving interactive segmentation models.",
+        help="Description of the workspace",
+    )
+    parser.add_argument(
+        "--owners", nargs="+", default=[], help="User IDs of workspace owners"
+    )
+    parser.add_argument(
+        "--allow_list",
+        nargs="+",
+        default=[],
+        help="User IDs allowed to access the workspace",
+    )
+    parser.add_argument(
+        "--deny_list",
+        nargs="+",
+        default=[],
+        help="User IDs denied access to the workspace",
+    )
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        default=False,
+        help="Overwrite the workspace if it already exists",
+    )
+    parser.add_argument(
+        "--token_expires_in",
+        default=31536000,
+        help="Token expiration time in seconds",  # 1 year
+    )
+
+    args = parser.parse_args()
+    asyncio.run(create_workspace_token(args))
