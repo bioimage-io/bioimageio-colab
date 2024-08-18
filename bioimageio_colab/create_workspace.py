343b19691d98f3a50990b745aa21eb90f9e19794
Nils Mechtel <nils.mech@gmail.com>
2024-08-18 16:33:39 +0200

    remove token generation

--- bioimageio_colab/create_workspace.py
+++ bioimageio_colab/create_workspace.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 from hypha_rpc import connect_to_server, login
 import asyncio
@@ -44,52 +43,8 @@ async def create_workspace_token(args):
             ]
         )
         print(f"Workspace created: {workspace['name']}")
-
-    # Generate a workspace token
-    token = await server.generate_token(
-        {
-            "workspace": args.workspace_name,
-            "expires_in": args.token_expires_in,
-            "permission": args.token_permission,
-            # "extra_scopes": [],
-        }
-    )
-    expires_in_days = args.token_expires_in / 60 / 60 / 24
-    print(
-        f"Workspace token generated:\n - Permission: '{args.token_permission}'\n - Expires in: {expires_in_days} days"
-    )
-
-    # Save the token to a .env file
-    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
-
-    # Read existing .env file if it exists
-    updated_env_lines = []
-    found_token_line = False
-    if os.path.exists(file_path):
-        with open(file_path, "r") as env_file:
-            for line in env_file:
-                if line.strip().startswith("WORKSPACE_TOKEN="):
-                    updated_env_lines.append(f'WORKSPACE_TOKEN="{token}"\n')
-                    found_token_line = True
-                else:
-                    updated_env_lines.append(line)
-
-    # If WORKSPACE_TOKEN line wasn't found, add it
-    if not found_token_line:
-        updated_env_lines.append(f'WORKSPACE_TOKEN="{token}"\n')
-
-    # Write back to the .env file
-    with open(file_path, "w") as env_file:
-        env_file.writelines(updated_env_lines)
-
-    # Define the permission mode for read and write for the owner only
-    mode = 0o600
-
-    # Change the file permissions
-    os.chmod(file_path, mode)
-
-    print(f"Token saved to {file_path}")
-
+    else:
+        print(f"Workspace already exists: {args.workspace_name}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -139,18 +94,6 @@ if __name__ == "__main__":
         action="store_true",
         default=False,
         help="Overwrite the workspace if it already exists",
-    )
-    parser.add_argument(
-        "--token_expires_in",
-        default=31536000,
-        type=int,
-        help="Token expiration time in seconds (default: 1 year)",
-    )
-    parser.add_argument(
-        "--token_permission",
-        default="read_write",
-        type=str,
-        help="Token permission (must be one of: read, read_write, admin)",
     )
 
     args = parser.parse_args()
