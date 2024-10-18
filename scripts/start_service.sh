959abde60785d527bb3b0c235692a47b59fccbc9
Nils Mechtel <nils.mech@gmail.com>
2024-10-18 23:55:37 +0200

    suppress root warning

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Upgrade hypha-rpc
-sudo pip install --upgrade hypha-rpc
+sudo pip install --upgrade hypha-rpc --root-user-action=ignore
 
 # Pass all arguments to the Python script
 python register_sam_service.py "$@"
