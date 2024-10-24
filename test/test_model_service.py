f69800bd3a637646ac50c62a4cf2b5128b03dbdb
Nils Mechtel <nils.mech@gmail.com>
2024-10-24 23:30:20 +0200

    register sam service with random client

--- test/test_model_service.py
+++ test/test_model_service.py
@@ -5,12 +5,11 @@ import requests
 
 SERVER_URL = "https://hypha.aicell.io"
 WORKSPACE_NAME = "bioimageio-colab"
-CLIENT_ID = "kubernetes"
 SERVICE_ID = "sam"
 
 
 def test_service_available():
-    service_url = f"{SERVER_URL}/{WORKSPACE_NAME}/services/{CLIENT_ID}:{SERVICE_ID}/hello"
+    service_url = f"{SERVER_URL}/{WORKSPACE_NAME}/services/*:{SERVICE_ID}/hello"
     response = requests.get(service_url)
     assert response.status_code == 200
     assert response.json() == "Welcome to the Interactive Segmentation service!"
@@ -19,8 +18,8 @@ def test_get_service():
     client = connect_to_server({"server_url": SERVER_URL, "method_timeout": 5})
     assert client
 
-    sid = f"{WORKSPACE_NAME}/{CLIENT_ID}:{SERVICE_ID}"
-    segment_svc = client.get_service(sid)
+    sid = f"{WORKSPACE_NAME}/*:{SERVICE_ID}"
+    segment_svc = client.get_service(sid, {"mode": "random"})
     assert segment_svc.id == sid
     assert segment_svc.config.workspace == WORKSPACE_NAME
     assert segment_svc.get("segment")
